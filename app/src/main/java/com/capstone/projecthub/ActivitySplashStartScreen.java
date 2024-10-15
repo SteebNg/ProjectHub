22e3bc5fc9e0f296d8d4779dac66303411f1e071
Steeb <ngweic1423@gmail.com>
2024-10-15 21:35:16 +0800

    Added Splash Start Screen

---
+++ app/src/main/java/com/capstone/projecthub/ActivitySplashStartScreen.java
@@ -0,0 +1,45 @@
+package com.capstone.projecthub;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.capstone.projecthub.databinding.ActivitySplashStartScreenBinding;
+
+public class ActivitySplashStartScreen extends AppCompatActivity {
+
+    private ActivitySplashStartScreenBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //set fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //force lightmode for user
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashStartScreenBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.startScreenMotionLayout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //delay and let the animation play before starting the home activity
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(ActivitySplashStartScreen.this, HomeActivity.class));
+            finish();
+        }, 4000);
+    }
+}
--- app/src/main/java/com/capstone/projecthub/HomeActivity.java
+++ app/src/main/java/com/capstone/projecthub/HomeActivity.java
@@ -8,13 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.capstone.projecthub.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
