a8b75b43a57b800f7cdbbfd471cceed02972ddb6
Steeb <ngweic1423@gmail.com>
2024-10-16 23:29:58 +0800

    Added Register Activity

---
+++ app/src/main/java/com/capstone/projecthub/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.capstone.projecthub;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.capstone.projecthub.databinding.ActivityRegisterBinding;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ActivityRegisterBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
