b4e21552264387803814034ee7bdc469e1d3350a
Steeb <ngweic1423@gmail.com>
2024-10-16 18:34:43 +0800

    Login Done. Left activity redirection.

--- app/src/main/java/com/capstone/projecthub/Constants.java
+++ app/src/main/java/com/capstone/projecthub/Constants.java
@@ -3,4 +3,6 @@ package com.capstone.projecthub;
 public class Constants {
 
     public static String KEY_PREFERENCE_NAME = "projectHubPreference";
+    public static String KEY_EMAIL = "email";
+    public static String KEY_PASSWORD = "password";
 }
--- app/src/main/java/com/capstone/projecthub/LoginActivity.java
+++ app/src/main/java/com/capstone/projecthub/LoginActivity.java
@@ -1,9 +1,19 @@
 package com.capstone.projecthub;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Patterns;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +23,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.capstone.projecthub.PreferenceManager.PreferenceManager;
 import com.capstone.projecthub.databinding.ActivityLoginBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -22,11 +33,21 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
-    //CAUTION: REMEMBER TO STORE USER DETAILS IN DATABASE TOO
-    //(TODO) Do a dialog that will check the internet connection status
-
     private ActivityLoginBinding binding;
     private FirebaseAuth auth;
+    private PreferenceManager preferenceManager;
+    private Button button;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //check if the user is signed in
+        //Please DO NOT USER FIREBASE DOCUMENTED METHOD TO CHECK
+        if (preferenceManager.getString(Constants.KEY_EMAIL) != null) {
+            //(TODO)redirect the user to Home
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +61,44 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
 
+        checkInternetAvailable();
         init();
         setListeners();
+    }
+
+    private void checkInternetAvailable() {
+        Handler handler = new Handler();
+
+        Runnable checkInternetTask = new Runnable() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (!InternetIsConnected() || !NetworkIsConnected()) {
+                            Dialog dialog = new Dialog(LoginActivity.this);
+                            dialog.setContentView(R.layout.no_internet_dialog);
+                            dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                            dialog.setCancelable(false);
+                            dialog.show();
+
+                            button = dialog.findViewById(R.id.buttonInternetRetry);
+                            button.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    dialog.dismiss();
+                                    finishAndRemoveTask(); //relaunch app
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        };
+
+        Thread threadForInternetStatus = new Thread(checkInternetTask);
+        threadForInternetStatus.start();
     }
 
     private void init() {
@@ -58,6 +115,8 @@ public class LoginActivity extends AppCompatActivity {
         binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                isLoginButtonLoading(true);
+
                 //check if the credentials are valid. Of course it is. What do you expect isCredentialsValid() gonna do??
                 boolean credentialsValid = isCredentialsValid();
 
@@ -66,23 +125,56 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+        //show password
+        binding.imageEyeHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.imageEyeHide.setVisibility(View.GONE);
+                binding.imageEyeShow.setVisibility(View.VISIBLE);
+                binding.editTextPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }
+        });
+        binding.imageEyeShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.imageEyeHide.setVisibility(View.VISIBLE);
+                binding.imageEyeShow.setVisibility(View.GONE);
+                binding.editTextPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            }
+        });
     }
 
     private void signIn() {
-        auth.signInWithEmailAndPassword(binding.editTextEmail.getText().toString(),
-                binding.editTextPassword.getText().toString())
+        String email = binding.editTextEmail.getText().toString().trim();
+        String password = binding.editTextPassword.getText().toString();
+
+        auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         //sign in is success
-                        //(TODO)store the value into preference manager
+                        preferenceManager.putString(Constants.KEY_EMAIL, email);
+                        preferenceManager.putString(Constants.KEY_PASSWORD, password);
+
+                        //(TODO)redirect to home activity
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                        isLoginButtonLoading(false);
                     }
                 });
+    }
+
+    private void isLoginButtonLoading(boolean isLoading) {
+        if (isLoading) {
+            binding.progressBarLogin.setVisibility(View.VISIBLE);
+            binding.buttonLogin.setText("");
+        } else {
+            binding.progressBarLogin.setVisibility(View.GONE);
+            binding.buttonLogin.setText("Login");
+        }
     }
 
     private boolean isCredentialsValid() {
@@ -103,6 +195,22 @@ public class LoginActivity extends AppCompatActivity {
             return false;
         } else {
             return true;
+        }
+    }
+
+    private boolean NetworkIsConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return connectivityManager.getActiveNetworkInfo() != null;
+    }
+
+    private boolean InternetIsConnected() {
+        try {
+            String command = "ping -c 1 google.com";
+            return (Runtime.getRuntime().exec(command).waitFor() == 0);
+        } catch (Exception e) {
+            return false;
         }
     }
 }
