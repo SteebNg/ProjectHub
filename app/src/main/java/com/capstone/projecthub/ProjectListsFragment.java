85fb9d55fea7ea45592f9686049ed526b33e487c
Steeb <ngweic1423@gmail.com>
2024-10-18 23:06:27 +0800

    Added navigation bar features for home

--- app/src/main/java/com/capstone/projecthub/ProjectListsFragment.java
+++ app/src/main/java/com/capstone/projecthub/ProjectListsFragment.java
@@ -15,12 +15,10 @@ import android.view.ViewGroup;
  */
 public class ProjectListsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -36,7 +34,6 @@ public class ProjectListsFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment ProjectListsFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static ProjectListsFragment newInstance(String param1, String param2) {
         ProjectListsFragment fragment = new ProjectListsFragment();
         Bundle args = new Bundle();
